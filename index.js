f42c0d9c1d788b9ce474de1aebc6a388b24738c8
reeFridge <reefridgerator@gmail.com>
2016-05-21 18:54:10 +0600

    Added simple usage case to index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-/**
- * Created by fridge on 5/21/16.
- */
+/** Simple usage node-torrent */
+var Client = require('node-torrent');
+var client = new Client({ logLevel: 'DEBUG'});
+var torrent = client.addTorrent('torrentFiles/crossed.torrent');
+
+torrent.on('complete', function() {
+	console.log('complete');
+	torrent.files.forEach(function(file) {
+		console.log(file.name);
+		console.log(file.path);
+	});
+});
